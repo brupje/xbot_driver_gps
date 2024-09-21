237c9dd668a2e5280ea97b095d1599a5d27a2594
brupje <info@bnhmeijer.nl>
2024-09-21 19:53:40 +0200

    Add 4

--- src/nmea_gps_interface.cpp
+++ src/nmea_gps_interface.cpp
@@ -53,6 +53,9 @@ xbot::driver::gps::NmeaGpsInterface::NmeaGpsInterface() : GpsInterface(), gps(pa
             case 3:
                 gps_state_.fix_type = GpsState::FixType::FIX_3D;
                 break;
+            case 4:
+                gps_state_.fix_type = GpsState::FixType::FIX_3D;
+                break;
             default:
                 gps_state_.fix_type = GpsState::FixType::NO_FIX;
                 break;
